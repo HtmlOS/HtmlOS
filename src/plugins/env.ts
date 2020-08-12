7808134ccb0243365185b97e7761e106b41324ce
Mr.Fan <fzm@synron.cn>
2020-08-12 12:35:59 +0800

    add markdown-it

--- src/plugins/env.ts
+++ src/plugins/env.ts
@@ -1,2 +1,35 @@
 const AppEnv: any = JSON.parse(process.env.VUE_APP_ENV || "{}");
+
+Object.defineProperty(Date.prototype, "format", {
+  value: function(fmt: string) {
+    const o: any = {
+      "M+": this.getMonth() + 1, //月份
+      "d+": this.getDate(), //日
+      "h+": this.getHours() % 12 == 0 ? 12 : this.getHours() % 12, //小时
+      "H+": this.getHours(), //小时
+      "m+": this.getMinutes(), //分
+      "s+": this.getSeconds(), //秒
+      "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+      S: this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) {
+      fmt = fmt.replace(
+        RegExp.$1,
+        (this.getFullYear() + "").substr(4 - RegExp.$1.length)
+      );
+    }
+    for (const k in o) {
+      if (new RegExp("(" + k + ")").test(fmt)) {
+        fmt = fmt.replace(
+          RegExp.$1,
+          RegExp.$1.length == 1
+            ? o[k]
+            : ("00" + o[k]).substr(("" + o[k]).length)
+        );
+      }
+    }
+    return fmt;
+  }
+});
+
 export default AppEnv;
--- src/views/blog/Blog.vue
+++ src/views/blog/Blog.vue
@@ -3,13 +3,17 @@
     <q-list bordered separator>
       <q-item v-for="(item, i) in blogs" :key="item.name" clickable v-ripple>
         <q-item-section clickable v-ripple @click="openblog(item)">
-          <q-item-label>{{ `${i}` }}</q-item-label>
-          <q-item-label>{{ `${item.created}` }}</q-item-label>
-          <p>
-            <code>{{ `${item.fixUrl(item.excerpt)}` }}</code>
-          </p>
-          <a :href="item.images[0].url">{{ `${item.images[0].url}` }}</a>
-          <a :href="item.file">{{ `${item.file}` }}</a>
+          <q-item-label>
+            {{ `${i + 1}` }}
+            <a :href="item.file">{{ `${item.file}` }}</a>
+          </q-item-label>
+          <q-item-label>
+            {{ `${item.created.format("yyyy年MM月dd日 HH:mm")}` }}
+          </q-item-label>
+          <markdown-it-vue-light
+            class="md-body"
+            :content="item.fixUrl(item.excerpt)"
+          />
         </q-item-section>
       </q-item>
     </q-list>
@@ -19,9 +23,13 @@
 <script>
 import { BlogManager } from "@/plugins/blog";
 import { LoveHeart } from "@/plugins/loveheart";
+
+import MarkdownItVueLight from "markdown-it-vue/dist/markdown-it-vue-light.umd.min.js";
+import "markdown-it-vue/dist/markdown-it-vue-light.css";
+
 export default {
   name: "Blog",
-  components: {},
+  components: { MarkdownItVueLight },
   data() {
     return {
       blogs: []
